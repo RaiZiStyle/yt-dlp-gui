7923a1a036fa2e2fbe1b3d3f3c478576e418eb5d
Arthur Guyot <agp+WSL@gmail.com>
2026-06-23 16:56:32 +0200

    Dirty commit, no more tram

--- main.py
+++ main.py
@@ -15,25 +15,4 @@ except IndexError:
 
 result = subprocess.run([YOUTUBE_DL_BINARY, "-J", url], capture_output=True, text=True)
 
-info = json.loads(result.stdout)
 
-for fmt in info["formats"]:
-    if fmt.get("ext") == "mhtml":
-        continue
-
-    resolution = fmt.get("resolution", "Unknown")
-    fps = fmt.get("fps", "Unknown")
-    filesize = fmt.get("filesize", 0)
-    if filesize is None:
-        filesize = 0
-    filesize_mb = filesize / (1024 * 1024)  # Convert bytes to MB
-    format = fmt.get("format", "Unknown")
-    print(f"Resulution : {resolution}, format : {format}, Size : {filesize_mb:.2f} MB, FPS : {fps}")
-
-    """
-    fmt important stuff : 
-    - format
-    - resolution
-    - fps
-    - filesize 
-    """
